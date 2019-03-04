ddea9fc4f6dcdef6968e7742b4745b2bf2b0cf52
Gil Pratte <gil.pratte@mutualmobile.com>
2019-03-03 19:15:20 -0600

    compareTo should delegate to the String compareTo

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,4 +1,5 @@
-public class Foo implements Comparable{
+
+public class Foo implements Comparable<Foo> {
     private final String name;
 
     public Foo(String name) {
@@ -9,10 +10,34 @@ public class Foo implements Comparable{
         return name;
     }
 
-    public int compareTo(Object o) {
+//    public int compareTo(Object o) {
+//
+//        if (o == null || !(o instanceof Foo)) {
+//            throw new IllegalArgumentException("cannot compare");
+//        }
+//
+//        Foo other = (Foo)o;
+//
+//        if (this == other){
+//            return 0;
+//        }
+//
+//        return this.name.compareTo(other.name);
+//    }
+
+    public int compareTo(Foo o) {
+
+        if (o == null) {
+            throw new IllegalArgumentException("cannot compare");
+        }
+
         if (this == o){
             return 0;
         }
-        return 1;
+
+        return this.name.compareTo(o.name);
     }
+
+
+
 }
